44447d45fc578a4aee1471b78f104b300da3d10b
Choi Geonu <6566gun@gmail.com>
2016-03-31 00:58:36 +0900

    Rearrange code

--- example-android/src/main/java/kr/geonu/example/updown/controller/UpDownController.kt
+++ example-android/src/main/java/kr/geonu/example/updown/controller/UpDownController.kt
@@ -10,19 +10,20 @@ class UpDownController : kr.geonu.mvc.ControllerMixin<UpDown> {
     internal class Reset : kr.geonu.mvc.Event
 
     override fun observeEvent(eventStream: Observable<kr.geonu.mvc.Event>): Observable<UpDown> =
-            Observable.just(UpDown(0)).mergeLatest(eventStream.flatMap { event ->
-                // Handle asynchronous event
-                when (event) {
-                    is UpDownView.ClickDelayedReset -> Observable.just(Reset()).delay(3, TimeUnit.SECONDS)
-                    else -> Observable.just(event)
-                }
-            }) { state, event ->
-                // Handle synchronous event
-                when (event) {
-                    is UpDownView.ClickUp -> state.copy(state.value + 1)
-                    is UpDownView.ClickDown -> state.copy(state.value - 1)
-                    is Reset -> UpDown(0)
-                    else -> state
-                }
-            }
+            Observable.just(UpDown(0))
+                    .mergeLatest(eventStream.flatMap { event ->
+                        // Handle asynchronous event
+                        when (event) {
+                            is UpDownView.ClickDelayedReset -> Observable.just(Reset()).delay(3, TimeUnit.SECONDS)
+                            else -> Observable.just(event)
+                        }
+                    }) { state, event ->
+                        // Handle synchronous event
+                        when (event) {
+                            is UpDownView.ClickUp -> state.copy(state.value + 1)
+                            is UpDownView.ClickDown -> state.copy(state.value - 1)
+                            is Reset -> UpDown(0)
+                            else -> state
+                        }
+                    }
 }
