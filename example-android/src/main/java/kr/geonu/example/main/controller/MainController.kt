231b574f8e5c2ff7ff42023f448719d32532d1f2
Choi Geonu <6566gun@gmail.com>
2016-03-31 17:10:29 +0900

    Init local blog activity

--- example-android/src/main/java/kr/geonu/example/main/controller/MainController.kt
+++ example-android/src/main/java/kr/geonu/example/main/controller/MainController.kt
@@ -3,6 +3,7 @@ package kr.geonu.example.main.controller
 import android.app.Activity
 import android.content.Intent
 import kr.geonu.example.activity.GitHubSearchActivity
+import kr.geonu.example.activity.PostListActivity
 import kr.geonu.example.activity.UpDownActivity
 import kr.geonu.example.main.model.ActivityList
 import kr.geonu.example.main.view.MainView
@@ -22,7 +23,8 @@ class MainController(val activity: Activity) : ControllerMixin<ActivityList> {
         }
         return Observable.just(ActivityList(listOf(
                 Pair("Up & Down", UpDownActivity::class.java),
-                Pair("GitHub Search", GitHubSearchActivity::class.java)
+                Pair("GitHub Search", GitHubSearchActivity::class.java),
+                Pair("Local Blog", PostListActivity::class.java)
         )))
     }
 }
